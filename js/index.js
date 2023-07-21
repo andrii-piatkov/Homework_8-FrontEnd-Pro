142fb5b8ae5ac66b8ad7ca470ba3ace259b83163
andrii-piatkov <baldygram.od@gmail.com>
2023-07-21 22:40:00 +0300

    last commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 function createCalculator(base) {
-  // implement
+  let num = base;
+
+  return {
+    add: function add(number) {
+      if (!isNaN(number)) {
+        num = num + number;
+      }
+    },
+    sub: function sub(number) {
+      if (!isNaN(number)) {
+        num = num - number;
+      }
+    },
+    set: function set(number) {
+      if (!isNaN(number)) {
+        num = number;
+      }
+    },
+    get: function get() {
+      return num;
+    },
+    reset: function reset() {
+      num = base;
+    },
+  };
 }
 
 const calculator = createCalculator(100);
@@ -10,8 +34,8 @@ calculator.sub(20);
 calculator.set(20);
 calculator.add(10);
 calculator.add(10);
-calculator.add("qwe"); // NaN и значение 40 не менять
-console.log(calculator.get()); // 40
+calculator.add("qwe");
+console.log(calculator.get());
 
 calculator.reset();
-console.log(calculator.get()); // 100
+console.log(calculator.get());
